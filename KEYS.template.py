7c94dc869b53155311e307a8719bb5c4b04a57b8
Valentin B <valentin.be@protonmail.com>
2020-08-18 17:10:18 +0200

    Improve USR_KEYS documentation

--- KEYS.template.py
+++ KEYS.template.py
@@ -2,13 +2,15 @@
 
 from binascii import unhexlify
 
-#: Keys that are derived by KeygenLdr by encrypting a seed with csecret 0x26
-#: and an auth signature, used to prevent potential attackers from tampering
-#: with the Boot and Keygen stages.
+#: Static keys that are derived by KeygenLdr to prevent potential attackers
+#: from tampering with the blobs or analyzing the code.
+#:
+#: The following algorithm derives these keys, using a variable seed:
+#: usr_key = aes_encrypt(aes_encrypt(csecret(0x26), get_seed()), auth_hash)
 USR_KEYS = [
     unhexlify("00000000000000000000000000000000"),  # CODE_SIG_01
     unhexlify("00000000000000000000000000000000"),  # CODE_ENC_01
 ]
 
 assert len(USR_KEYS) == 2
-assert all(map(lambda k: any(k), USR_KEYS)), "Please provide real keys in order to continue"
+assert all(map(any, USR_KEYS)), "Please provide real keys in order to continue"
